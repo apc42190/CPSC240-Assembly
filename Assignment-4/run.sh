de80423cf469b75d5bb0f3d4ec0bde2c31e6c34d
Aaron Cope <apc42190@yahoo.com>
2021-10-14 23:29:53 -0700

    Add files via upload

---
+++ Assignment-4/run.sh
@@ -0,0 +1,12 @@
+rm *.out
+echo "Assemble hertz.asm"
+nasm -f elf64 -l hertz.lis -o hertz.o hertz.asm
+echo "Compile maxwell.cpp"
+g++ -c -Wall -m64 -no-pie -o maxwell.o maxwell.c -std=c++17
+echo "Link object files"
+g++ -m64 -no-pie -o maxwell.out -std=c++17 hertz.o maxwell.o is_float.cpp
+echo "------Program Start------"
+./maxwell.out
+echo "------Program End------"
+rm *.o
+rm *.lis
